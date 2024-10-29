59dc5869d336d06e5595ce03adb324f3f8ea466a
Hyeonyong16 <49775204+Hyeonyong16@users.noreply.github.com>
2024-10-29 22:22:13 +0900

    Player Move, Shot 기능 추가
    1. W, A, S, D 키 입력을 통한 플레이어 이동 구현
        - 키 중복 시 속도가 빨라지는 현상 수정
        - 초기 속도, 마찰력, 최대 속도 수정
    
    2. 화살표 버튼을 통한 플레이어 공격 추가
        - 각 방향 키 입력 시 해당 방향으로 공격 기능 추가
        - 방향 키 중복 입력 시 가장 먼저 들어온 키 방향으로 공격 유지
        - 중복 입력 중 키 땔 시 먼저 입력 들어온 방향으로 공격
        - Tap 반복시에도 공격 속도에 맞게 공격하도록 수정

--- WinAPI/WinAPI/struct.h
+++ WinAPI/WinAPI/struct.h
@@ -45,6 +45,12 @@ public:
 	void operator /= (Vec2 _Other) { assert(_Other.x && _Other.y); x /= _Other.x; y /= _Other.y; }
 	void operator /= (float _f) { assert(_f); x /= _f; y /= _f; }
 
+	bool operator != (Vec2 _Other)
+	{
+		if (x == _Other.x && y == _Other.y) return false;
+		else return true;
+	}
+
 public:
 	Vec2()
 		: x(0.f)
